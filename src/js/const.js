4c0ae40d62fe3ead582432250ca41cc96a3020e2
AlexeyKonstantinov78 <alex_kav@mail.ru>
2024-03-22 14:13:59 +0300

    update js module

---
+++ src/js/const.js
@@ -0,0 +1,2 @@
+export const API_URL = 'https://blush-great-flavor.glitch.me';
+
--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,7 @@
-'use strict';
+import { toppingsToogle } from './toppingsButtom.js';
 
-const toppingsButton = document.querySelector('.toppings__button');
-const toppingsList = document.querySelector('.toppings__list');
+const init = () => {
+  toppingsToogle();
+};
 
-toppingsButton.addEventListener('click', () => {
-
-  if (!toppingsList.classList.contains('toppings__list_show')) {
-    toppingsList.classList.add('toppings__list_show');
-    toppingsButton.classList.add('toppings__button_active');
-    toppingsList.style.maxHeight = toppingsList.scrollHeight + 'px';
-  } else {
-    toppingsButton.classList.remove('toppings__button_active');
-    toppingsList.style.maxHeight = null;
-
-    setTimeout(() => {
-      toppingsList.classList.remove('toppings__list_show');
-    }, 360);
-  }
-
-});
+init();
