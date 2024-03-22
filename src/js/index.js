23c0e62f39124ba778dec341a3d232f992930feb
AlexeyKonstantinov78 <alex_kav@mail.ru>
2024-03-22 15:03:17 +0300

    render list pizza

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
+import { renderPizza } from './renderPizza.js';
 import { toppingsToogle } from './toppingsButtom.js';
 
 const init = () => {
   toppingsToogle();
+  renderPizza();
 };
 
 init();
---
+++ src/js/renderPizza.js
@@ -0,0 +1,48 @@
+import { API_URL } from './const.js';
+
+const getPizzas = async () => {
+  try {
+    const response = await fetch(`${API_URL}/api/products`);
+    if (!response.ok) {
+      throw new Error('Опс что то пошло не так');
+    }
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const createCard = data => {
+  const card = document.createElement('article');
+  card.classList.add('card');
+
+  card.innerHTML = `
+    <picture>
+      <source srcset="${data.images[1]}" type="image/webp">
+      <img class="card__image" src="${data.images[0]}" alt="${data.name.ru}">
+    </picture>
+    <div class="card__content">
+        <h3 class="card__title">${data.name.ru}</h3>
+        <p class="card__info"><span class="card__price">${data.price['25cm']} &#x20bd;</span><span>/</span><span class="card__weight">25 см</span></p>
+        <button class="card__button" data-id="${data.id}">Выбрать</button>
+    </div>
+  `;
+
+  return card;
+};
+
+export const renderPizza = async () => {
+  const pizzas = await getPizzas();
+  const pizzaList = document.querySelector('.pizza__list');
+  pizzaList.textContent = '';
+
+  const itemList = pizzas.map((data) => {
+    const item = document.createElement('li');
+    item.classList.add('pizza__item');
+    const card = createCard(data);
+    item.append(card);
+    return item;
+  });
+
+  pizzaList.append(...itemList);
+};
