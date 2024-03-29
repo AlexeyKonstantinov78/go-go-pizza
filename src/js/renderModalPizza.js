34d9dc967faa623cb01f787c1a65f54f3619c4cd
Aleksey Konstantinov Ubunty <alex_kav@mail.ru>
2024-03-29 15:25:00 +0300

    add modal input value change

--- src/js/renderModalPizza.js
+++ src/js/renderModalPizza.js
@@ -8,8 +8,8 @@ const renderELement = (data) => {
   modalPizzaMain.innerHTML = `
     <img class="modal-pizza__img" width="180" height="180" src="${data.images[1]}" alt="${data.name.ru}">
     <h2 class="modal-pizza__title">${textToLoverToUpperCase(data.name.ru)}</h2>
-    <p class="modal-pizza__toppings">${data.toppings.ru}</p>
-    <p class="modal-pizza__info"><span class="modal-pizza__price">${data.price['25cm']} &#x20bd;</span><span>/</span><span class="modal-pizza__size">25 см</span></p>
+    <p class="modal-pizza__toppings">${textToLoverToUpperCase(data.toppings.ru)}</p>
+    <p class="modal-pizza__info"><span class="modal-pizza__price">${data.price['25cm']}</span>  &#x20bd;<span>/</span><span class="modal-pizza__size">25 см</span></p>
     <form class="modal-pizza__form">
         <div class="modal-pizza__group-fieldset">
             <fieldset class="modal-pizza__fieldset">
@@ -19,11 +19,11 @@ const renderELement = (data) => {
                 <label class="modal-pizza__label" for="thin">Тонкое тесто</label>
             </fieldset>
             <fieldset class="modal-pizza__fieldset">
-                <input class="modal-pizza__radio" type="radio" id="25см" name="size" value="25см" checked>
+                <input class="modal-pizza__radio" type="radio" id="25см" name="size" value="25cm" checked>
                 <label class="modal-pizza__label" for="25см">25 см</label>
-                <input class="modal-pizza__radio" type="radio" id="30см" name="size" value="30см">
+                <input class="modal-pizza__radio" type="radio" id="30см" name="size" value="30cm">
                 <label class="modal-pizza__label" for="30см">30 см</label>
-                <input class="modal-pizza__radio" type="radio" id="35см" name="size" value="35см">
+                <input class="modal-pizza__radio" type="radio" id="35см" name="size" value="35cm">
                 <label class="modal-pizza__label" for="35см">35 см</label>
             </fieldset>
         </div>
@@ -37,11 +37,34 @@ const renderELement = (data) => {
   `;
 }
 
+const changeInputValue = (target, data) => {
+  const modalPizzaPrice = document.querySelector('.modal-pizza__price');
+  const size = target.value;
+
+  modalPizzaPrice.textContent = data[size];
+
+  // for (const key in data) {
+  //   if (key === size) {
+  //     modalPizzaPrice.textContent = data[key];
+  //     break;
+  //   }
+  //   modalPizzaPrice.textContent = '';
+  // }
+};
+
 export const renderModalPizza = async (id) => {
-  console.log('id: ', id);
   const dataIdPizza = await getData(`/api/products/${id}`);
-  console.log('dataIdPizza: ', dataIdPizza);
   if (dataIdPizza) {
     renderELement(dataIdPizza);
   }
+
+  const modalPizzaForm = document.querySelector('.modal-pizza__form');
+
+  modalPizzaForm.addEventListener('change', (event) => {
+    const target = event.target;
+
+    if (target.name === 'size') {
+      changeInputValue(target, dataIdPizza.price);
+    }
+  })
 };
